9e16d778cf97f86c1537bef7fbb83710476a8be8
yuhandeng <moliflower233@163.com>
2026-04-17 16:12:33 +0800

    add test

--- test/test_binary.py
+++ test/test_binary.py
@@ -1,7 +1,13 @@
 import numpy as np  # noqa: F401
 import pytest
 import torch
-from scipy.ndimage import binary_erosion, generate_binary_structure
+from scipy.ndimage import (
+    binary_closing,
+    binary_dilation,
+    binary_erosion,
+    binary_opening,
+    generate_binary_structure,
+)
 
 import torchmorph as tm
 
@@ -41,8 +47,9 @@ def apply_scipy_to_batch(np_input, scipy_func, **kwargs):
     return output
 
 
-def batch_scipy_erosion(
+def batch_scipy(
     np_input: np.ndarray,
+    scipy_func: callable,
     structure: np.ndarray | None = None,
     iterations: int = 1,
     mask: np.ndarray | None = None,
@@ -52,7 +59,7 @@ def batch_scipy_erosion(
 ) -> np.ndarray:
     return apply_scipy_to_batch(
         np_input,
-        binary_erosion,
+        scipy_func,
         structure=structure,
         iterations=iterations,
         mask=mask,
@@ -84,32 +91,181 @@ case_structure_4d = generate_binary_structure(rank=4, connectivity=4)
 
 
 @pytest.mark.parametrize(
-    ("np_input, structure, iterations, origin, border_value"),
+    ("np_input, scipy_func, structure, iterations, origin, border_value"),
     [
-        pytest.param(case_2d, None, 1, 0, False, id="2D_basic"),
-        pytest.param(case_2d, case_structure_2d, 1, 0, False, id="2D_structure"),
-        pytest.param(case_2d, None, 2, 0, False, id="2D_1iterations"),
-        pytest.param(case_2d, None, -1, 0, False, id="2D_-1iterations"),
-        pytest.param(case_2d, None, 1, 1, False, id="2D_1origin"),
-        pytest.param(case_2d, None, 1, 1, True, id="2D_1origin"),
-        pytest.param(case_3d, None, 1, 0, False, id="3D_basic"),
-        pytest.param(case_3d, case_structure_3d_1, 1, 0, False, id="3D_2Dstructure"),
-        pytest.param(case_3d, case_structure_3d_2, 1, 0, False, id="3D_3Dstructure"),
-        pytest.param(case_3d, None, 2, 0, False, id="3D_1iterations"),
-        pytest.param(case_3d, None, -1, 0, False, id="3D_-1iterations"),
-        pytest.param(case_3d, None, 1, 1, False, id="3D_1origin"),
-        pytest.param(case_3d, None, 1, 1, True, id="3D_1origin"),
-        pytest.param(case_4d, None, 1, 0, False, id="4D_basic"),
-        pytest.param(case_4d, case_structure_4d, 1, 0, False, id="4D_4Dstructure"),
-        pytest.param(case_4d, case_structure_4d, 1, 0, False, id="4D_4Dstructure"),
-        pytest.param(case_4d, None, 2, 0, False, id="4D_1iterations"),
-        pytest.param(case_4d, None, -1, 0, False, id="4D_-1iterations"),
-        pytest.param(case_4d, None, 1, 1, False, id="4D_1origin"),
-        pytest.param(case_4d, None, 1, 1, True, id="4D_1origin"),
+        pytest.param(case_2d, binary_erosion, None, 1, 0, False, id="erosion_2D_basic"),
+        pytest.param(
+            case_2d, binary_erosion, case_structure_2d, 1, 0, False, id="erosion_2D_structure"
+        ),
+        pytest.param(case_2d, binary_erosion, None, 2, 0, False, id="erosion_2D_2iterations"),
+        pytest.param(case_2d, binary_erosion, None, -1, 0, False, id="erosion_2D_-1iterations"),
+        pytest.param(
+            case_2d, binary_erosion, None, 1, 1, False, id="erosion_2D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_2d, binary_erosion, None, 1, 1, True, id="erosion_2D_1origin_border_value=True"
+        ),
+        pytest.param(case_3d, binary_erosion, None, 1, 0, False, id="erosion_3D_basic"),
+        pytest.param(
+            case_3d, binary_erosion, case_structure_3d_1, 1, 0, False, id="erosion_3D_2Dstructure1"
+        ),
+        pytest.param(
+            case_3d, binary_erosion, case_structure_3d_2, 1, 0, False, id="erosion_3D_3Dstructure2"
+        ),
+        pytest.param(case_3d, binary_erosion, None, 2, 0, False, id="erosion_3D_2iterations"),
+        pytest.param(case_3d, binary_erosion, None, -1, 0, False, id="erosion_3D_-1iterations"),
+        pytest.param(
+            case_3d, binary_erosion, None, 1, 1, False, id="erosion_3D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_3d, binary_erosion, None, 1, 1, True, id="erosion_3D_1origin_border_value=True"
+        ),
+        pytest.param(case_4d, binary_erosion, None, 1, 0, False, id="erosion_4D_basic"),
+        pytest.param(
+            case_4d, binary_erosion, case_structure_4d, 1, 0, False, id="erosion_4D_4Dstructure"
+        ),
+        pytest.param(case_4d, binary_erosion, None, 2, 0, False, id="erosion_4D_2iterations"),
+        pytest.param(case_4d, binary_erosion, None, -1, 0, False, id="erosion_4D_-1iterations"),
+        pytest.param(
+            case_4d, binary_erosion, None, 1, 1, False, id="erosion_4D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_4d, binary_erosion, None, 1, 1, True, id="erosion_4D_1origin_border_value=True"
+        ),
+        pytest.param(case_2d, binary_dilation, None, 1, 0, False, id="dilation_2D_basic"),
+        pytest.param(
+            case_2d, binary_dilation, case_structure_2d, 1, 0, False, id="dilation_2D_structure"
+        ),
+        pytest.param(case_2d, binary_dilation, None, 2, 0, False, id="dilation_2D_2iterations"),
+        pytest.param(case_2d, binary_dilation, None, -1, 0, False, id="dilation_2D_-1iterations"),
+        pytest.param(
+            case_2d, binary_dilation, None, 1, 1, False, id="dilation_2D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_2d, binary_dilation, None, 1, 1, True, id="dilation_2D_1origin_border_value=True"
+        ),
+        pytest.param(case_3d, binary_dilation, None, 1, 0, False, id="dilation_3D_basic"),
+        pytest.param(
+            case_3d,
+            binary_dilation,
+            case_structure_3d_1,
+            1,
+            0,
+            False,
+            id="dilation_3D_2Dstructure1",
+        ),
+        pytest.param(
+            case_3d,
+            binary_dilation,
+            case_structure_3d_2,
+            1,
+            0,
+            False,
+            id="dilation_3D_3Dstructure2",
+        ),
+        pytest.param(case_3d, binary_dilation, None, 2, 0, False, id="dilation_3D_2iterations"),
+        pytest.param(case_3d, binary_dilation, None, -1, 0, False, id="dilation_3D_-1iterations"),
+        pytest.param(
+            case_3d, binary_dilation, None, 1, 1, False, id="dilation_3D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_3d, binary_dilation, None, 1, 1, True, id="dilation_3D_1origin_border_value=True"
+        ),
+        pytest.param(case_4d, binary_dilation, None, 1, 0, False, id="dilation_4D_basic"),
+        pytest.param(
+            case_4d, binary_dilation, case_structure_4d, 1, 0, False, id="dilation_4D_4Dstructure"
+        ),
+        pytest.param(case_4d, binary_dilation, None, 2, 0, False, id="dilation_4D_2iterations"),
+        pytest.param(case_4d, binary_dilation, None, -1, 0, False, id="dilation_4D_-1iterations"),
+        pytest.param(
+            case_4d, binary_dilation, None, 1, 1, False, id="dilation_4D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_4d, binary_dilation, None, 1, 1, True, id="dilation_4D_1origin_border_value=True"
+        ),
+        pytest.param(case_2d, binary_opening, None, 1, 0, False, id="opening_2D_basic"),
+        pytest.param(
+            case_2d, binary_opening, case_structure_2d, 1, 0, False, id="opening_2D_structure"
+        ),
+        pytest.param(case_2d, binary_opening, None, 2, 0, False, id="opening_2D_2iterations"),
+        pytest.param(case_2d, binary_opening, None, -1, 0, False, id="opening_2D_-1iterations"),
+        pytest.param(
+            case_2d, binary_opening, None, 1, 1, False, id="opening_2D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_2d, binary_opening, None, 1, 1, True, id="opening_2D_1origin_border_value=True"
+        ),
+        pytest.param(case_3d, binary_opening, None, 1, 0, False, id="opening_3D_basic"),
+        pytest.param(
+            case_3d, binary_opening, case_structure_3d_1, 1, 0, False, id="opening_3D_2Dstructure1"
+        ),
+        pytest.param(
+            case_3d, binary_opening, case_structure_3d_2, 1, 0, False, id="opening_3D_3Dstructure2"
+        ),
+        pytest.param(case_3d, binary_opening, None, 2, 0, False, id="opening_3D_2iterations"),
+        pytest.param(case_3d, binary_opening, None, -1, 0, False, id="opening_3D_-1iterations"),
+        pytest.param(
+            case_3d, binary_opening, None, 1, 1, False, id="opening_3D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_3d, binary_opening, None, 1, 1, True, id="opening_3D_1origin_border_value=True"
+        ),
+        pytest.param(case_4d, binary_opening, None, 1, 0, False, id="opening_4D_basic"),
+        pytest.param(
+            case_4d, binary_opening, case_structure_4d, 1, 0, False, id="opening_4D_4Dstructure"
+        ),
+        pytest.param(case_4d, binary_opening, None, 2, 0, False, id="opening_4D_2iterations"),
+        pytest.param(case_4d, binary_opening, None, -1, 0, False, id="opening_4D_-1iterations"),
+        pytest.param(
+            case_4d, binary_opening, None, 1, 1, False, id="opening_4D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_4d, binary_opening, None, 1, 1, True, id="opening_4D_1origin_border_value=True"
+        ),
+        pytest.param(case_2d, binary_closing, None, 1, 0, False, id="opening_2D_basic"),
+        pytest.param(
+            case_2d, binary_closing, case_structure_2d, 1, 0, False, id="opening_2D_structure"
+        ),
+        pytest.param(case_2d, binary_closing, None, 2, 0, False, id="opening_2D_2iterations"),
+        pytest.param(case_2d, binary_closing, None, -1, 0, False, id="opening_2D_-1iterations"),
+        pytest.param(
+            case_2d, binary_closing, None, 1, 1, False, id="opening_2D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_2d, binary_closing, None, 1, 1, True, id="opening_2D_1origin_border_value=True"
+        ),
+        pytest.param(case_3d, binary_closing, None, 1, 0, False, id="opening_3D_basic"),
+        pytest.param(
+            case_3d, binary_closing, case_structure_3d_1, 1, 0, False, id="opening_3D_2Dstructure1"
+        ),
+        pytest.param(
+            case_3d, binary_closing, case_structure_3d_2, 1, 0, False, id="opening_3D_3Dstructure2"
+        ),
+        pytest.param(case_3d, binary_closing, None, 2, 0, False, id="opening_3D_2iterations"),
+        pytest.param(case_3d, binary_closing, None, -1, 0, False, id="opening_3D_-1iterations"),
+        pytest.param(
+            case_3d, binary_closing, None, 1, 1, False, id="opening_3D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_3d, binary_closing, None, 1, 1, True, id="opening_3D_1origin_border_value=True"
+        ),
+        pytest.param(case_4d, binary_closing, None, 1, 0, False, id="opening_4D_basic"),
+        pytest.param(
+            case_4d, binary_closing, case_structure_4d, 1, 0, False, id="opening_4D_4Dstructure"
+        ),
+        pytest.param(case_4d, binary_closing, None, 2, 0, False, id="opening_4D_2iterations"),
+        pytest.param(case_4d, binary_closing, None, -1, 0, False, id="opening_4D_-1iterations"),
+        pytest.param(
+            case_4d, binary_closing, None, 1, 1, False, id="opening_4D_1origin_border_value=False"
+        ),
+        pytest.param(
+            case_4d, binary_closing, None, 1, 1, True, id="opening_4D_1origin_border_value=True"
+        ),
     ],
 )
-def test_binary_erosion_basic(
+def test_binary_basic(
     np_input,
+    scipy_func,
     structure,
     iterations,
     origin,
@@ -120,16 +276,17 @@ def test_binary_erosion_basic(
         structure_cuda = torch.as_tensor(structure, dtype=torch.float32)
     else:
         structure_cuda = None
-
-    actual = tm.binary_erosion(
+    tm_func = getattr(tm, scipy_func.__name__)
+    actual = tm_func(
         x,
         structure=structure_cuda,
         iterations=iterations,
         origin=origin,
         border_value=border_value,
     )
-    expected_np = batch_scipy_erosion(
+    expected_np = batch_scipy(
         np_input,
+        scipy_func,
         structure=structure,
         iterations=iterations,
         origin=origin,
